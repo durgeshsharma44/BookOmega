3459c7c8f3dccaf22f7330cbba745ec3fcd58f0c
Abhishek Kumar <kumar.abhishek1@incedoinc.com>
2025-03-02 18:25:23 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,52 +1,31 @@
 const mongoose=require('mongoose')
-const URL="mongodb+srv://durgeshsharma:durgesh123@dicdatabase.igctqhh.mongodb.net/bookomega?retryWrites=true&w=majority"
+const dotenv = require('dotenv');
 
-const mongoDB=async()=>{
-    // await mongoose.connect(URL,{useNewURLParser:true},async(err,result)=>{
-    //  if(err)console.log(err);
-    //  else{
-    //  console.log("connected");
-    //  const fetched_data=await mongoose.connection.db.collection("products");
-    //  fetched_data.find({}).toArray( function(err,data){
-    //     if(err)console.log(err);
-    //     else console.log(data);
-    //  });
-    //  }
-    // });
+dotenv.config();
+const mongoURI = process.env.MONGO_URL;
 
-// await mongoose.connect(URL)
-// .then(async()=>{
-// console.log("connected");
-// const fetched_data=await mongoose.connection.db.collection("test");
-// fetched_data.find({}).toArray( function(err,data){
-//     if(err)console.log(err);
-//     else console.log(data);
-//  });
-// })
-// .catch((e)=>{console.log(e)})
-
-await mongoose.connect(URL)
-.then(async () => {
-  console.log("connected");
-
-  const fetchedData = await mongoose.connection.db.collection("users");
-
-  fetchedData.find({}).toArray((err, data) => {
-    if (err) {
-      console.error("Error retrieving data:", err);
-    } else {
-      console.log(data);
-    }
-  });
-})
-
-.catch((e) => {
-  console.error("Error connecting to MongoDB:", e);
-});
-
+if (!mongoURI) {
+  console.error('Error: MONGO_URI is not defined in .env');
+  process.exit(1);
 }
 
-
-
-
-module.exports=mongoDB();
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(mongoURI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+      process.exit(1);
+    }
+  };
+  const closeDB = async () => {
+    try {
+      await mongoose.connection.close();
+      console.log('MongoDB connection closed');
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+    }
+  };
+  
+  module.exports= connectDB;
+  
