bbfba9cfc412e6e19cbf681f6e6ab5c79a7f566b
Durgesh Sharma <118758988+durgeshsharma44@users.noreply.github.com>
2025-03-02 13:01:53 +0530

    jn

--- bookomega/src/components/Home.js
+++ bookomega/src/components/Home.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import Cards from './Cards'
 import Carousel from './Carousel'
-import styled from 'styled-components'
 import Subject from './Subject'
+import Footer from './Footer'
+import styled from 'styled-components'
+
+// Styled component for better control of alignment and styling
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const AboutSection = styled.div`
+  margin-top: 40px;
+  padding: 20px;
+  max-width: 800px;
+  background-color: #f8f9fa;
+  border-radius: 8px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
 export default function Home() {
     return (
         <div>
-            <Carousel/>
-                <Subject/>
-                
-               
-                
+            <Carousel />
+            <Subject />
+            <ContentWrapper>
+                <AboutSection>
+                    <h5>About BookSwap</h5>
+                    <p>Your trusted platform to buy and sell second-hand books. Find books at affordable prices and give your old books a new home.</p>
+                </AboutSection>
+            </ContentWrapper>
+            <Footer />
         </div>
     )
 }
--- bookomega/src/components/Subject.js
+++ bookomega/src/components/Subject.js
@@ -1,45 +1,47 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+
 export default function Subject() {
-
-  
-
   return (
-    <div className='subjects'>
-       <div className="card bg-secondary" style={{"width": "18rem" , "maxHeight":"360px"}} >
-   <img style={{"width": "100%" , "maxHeight":"257px"}} src="../../images/jee.png" className="card-img-top" alt="..."/>
-  <div className="card-body" >
-    {/* <button onClick={Selectsubject("jee")} className='btn'>Explore</button> */}
-    <Link class="btn" to="/jee">Explore</Link>
-  </div>
-  </div>
+    <div className="container my-4">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div className="card bg-secondary" style={{ width: "100%", maxHeight: "360px" }}>
+            <img style={{ width: "100%", maxHeight: "257px" }} src="../../images/jee.png" className="card-img-top" alt="JEE" />
+            <div className="card-body">
+              <Link className="btn" to="/jee">Explore</Link>
+            </div>
+          </div>
+        </div>
 
-  <div className="card bg-secondary" style={{"width": "18rem" , "maxHeight":"360px"}} >
-   <img style={{"width": "100%" , "maxHeight":"257px"}} src="../../images/ssc.jpeg" className="card-img-top" alt="..."/>
-  <div className="card-body " >
-    {/* <button onClick={Selectsubject("Neet")} className='btn'>Explore</button> */}
-    <Link class="btn" to="/neet">Explore</Link>
-  </div>
-  </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div className="card bg-secondary" style={{ width: "100%", maxHeight: "360px" }}>
+            <img style={{ width: "100%", maxHeight: "257px" }} src="../../images/ssc.jpeg" className="card-img-top" alt="SSC" />
+            <div className="card-body">
+              <Link className="btn" to="/neet">Explore</Link>
+            </div>
+          </div>
+        </div>
 
-  <div className="card bg-secondary" style={{"width": "18rem" , "maxHeight":"360px"}} >
-   <img  style={{"width": "100%" , "maxHeight":"257px"}} src="../../images/neet.png" className="card-img-top" alt="..."/>
-  <div className="card-body" >
-    {/* <button onClick={Selectsubject("upsc")} className='btn'>Explore</button> */}
-    <Link class="btn" to="/ssc">Explore</Link>
-  </div>
-  </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div className="card bg-secondary" style={{ width: "100%", maxHeight: "360px" }}>
+            <img style={{ width: "100%", maxHeight: "257px" }} src="../../images/neet.png" className="card-img-top" alt="NEET" />
+            <div className="card-body">
+              <Link className="btn" to="/ssc">Explore</Link>
+            </div>
+          </div>
+        </div>
 
-  <div className="card bg-secondary" style={{"width": "18rem" , "maxHeight":"360px"}} >
-   <img  style={{"width": "100%" , "maxHeight":"257px"}} src="../../images/upsc.jpeg" className="card-img-top" alt="..."/>
-  <div className="card-body" >
-    {/* <button onClick={Selectsubject("ssc")} className='btn'>Explore</button> */}
-    <Link class="btn" to="/upsc">Explore</Link>
-  </div>
-  </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div className="card bg-secondary" style={{ width: "100%", maxHeight: "360px" }}>
+            <img style={{ width: "100%", maxHeight: "257px" }} src="../../images/upsc.jpeg" className="card-img-top" alt="UPSC" />
+            <div className="card-body">
+              <Link className="btn" to="/upsc">Explore</Link>
+            </div>
+          </div>
+        </div>
 
-
+      </div>
     </div>
   )
 }
